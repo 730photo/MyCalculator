437139f0c9e66df41c5876f14c53ed1c66bbd09f
architekespy <architekespy@Reginalds-MacBook-Pro.local>
2019-01-30 19:39:12 -0500

    coming along nicely

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Button.css'; 
+
+//tells us that it is an operator
+const isOperator = val => {
+    return !isNaN(val) || val === "." || val === "="
+}
+
+export const Button = props => 
+    <div className={`button-wrapper ${
+        isOperator(props.children) ? null : "operator"
+    }`}>
+        {/* pass the opening tag and the closing tag of out button component and whatever is in the middle will be passed in here */}
+        {props.children}
+    </div>
+
+
