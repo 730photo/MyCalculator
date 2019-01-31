0004a37fca72bdbc3799ac626eaa850272c5ce41
architekespy <architekespy@Reginalds-MacBook-Pro.local>
2019-01-30 21:01:44 -0500

    basically finished, will update README later

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,7 +9,9 @@ const isOperator = val => {
 export const Button = props => 
     <div className={`button-wrapper ${
         isOperator(props.children) ? null : "operator"
-    }`}>
+    }`}
+    onClick={() => props.handleClick(props.children)}
+    >
         {/* pass the opening tag and the closing tag of out button component and whatever is in the middle will be passed in here */}
         {props.children}
     </div>
